8bd371102738005dc65eb5bcba046933a9dc1299
gpeng <michael.gang.peng@gmail.com>
2020-06-01 18:03:52 -0400

    Initial Commit

---
+++ fstCalculation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fstCalculation
+//
+//  Created by Gang Peng on 6/1/20.
+//  Copyright © 2020 Gang Peng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
